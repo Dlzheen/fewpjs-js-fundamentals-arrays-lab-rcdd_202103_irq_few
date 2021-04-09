736438a4cbd56ca0ef89bfd88d5568112711798f
Dlzheen Mahmood <dlzheenmahmood@gmail.com>
2021-04-09 18:10:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const append = ["Milo", "Otis", "Gsrfield"];
 const prepend = ["Milo", "Otis", "Gsrfield"];
 const removeLast = ["Milo", "Otis", "Gsrfield"];
 const removeFirst = ["Milo", "Otis", "Gsrfield"];
- 
+append.push("Odie");
+prepend.unshift("Odie");
